9f3e2e5c88fe769ca1209fa1e3767597ec5dcaed
junaid-h0ssain <158271661+junaid-h0ssain@users.noreply.github.com>
2025-05-29 03:23:26 +0600

    made classes

--- entities.py
+++ entities.py
@@ -3,38 +3,37 @@ import pygame
 screenHight = 720
 screenWidth = 1280
 screen = pygame.display.set_mode((screenWidth,screenHight))
-rb = 1
+
 
 class player:
     def __init__(self,x,y):
         self.x = x
         self.y = y
-    
-    
-    img = pygame.image.load('images/player.png').convert_alpha()
-    frect = img.get_frect(center = (screenWidth/2,screenHight/2))
-    #frect = img.get_frect(center = ()))
-    x_pos = 0
-    y_pos = 0    
+        self.img = pygame.image.load('images/player.png').convert_alpha()
+        # frect = img.get_frect(center = (screenWidth/2,screenHight/2))
+        self.frect = self.img.get_frect(center = (x,y))
+        self.rb = 1
+        # x_pos = 0
+        # y_pos = 0    
 
     def bounce1(self):
-        global rb
-        if player.frect.right <screenWidth and not rb:
-            player.frect.left+=1
+        
+        if self.frect.right <screenWidth and not self.rb:
+            self.frect.left+=1
             # print(player.frect.right)
-            if player.frect.right==screenWidth:
-                rb = 1
-        if player.frect.left >0 and rb:
-            player.frect.right-=1
+            if self.frect.right==screenWidth:
+                self.rb = 1
+        if self.frect.left >0 and self.rb:
+            self.frect.right-=1
             # print(player.frect.right)
-            if player.frect.left==0:
-                rb = 0
+            if self.frect.left==0:
+                self.rb = 0
     
     def bounce2(self):
-        global rb
-        player.frect.x += rb * 1
-        if player.frect.right>screenWidth or player.frect.left<0:
-            rb*=-1
+        
+        self.frect.x += self.rb * 1
+        if self.frect.right>screenWidth or self.frect.left<0:
+            self.rb*=-1
 
 class star:
     img = pygame.image.load('images/star.png').convert_alpha()
--- main.py
+++ main.py
@@ -7,24 +7,27 @@ screenHight = 720
 screenWidth = 1280
 screen = pygame.display.set_mode((screenWidth,screenHight))
 run = True
-
+p1 = player(100,100)
+p2 = player(200,200)
 
 while run:
-    pygame.display.set_caption(f'{player.frect}')
+    pygame.display.set_caption(f'{p1.frect}')
     # pygame.display.set_caption(str(pygame.display.get_current_refresh_rate()))
     keys = pygame.key.get_pressed()
     for event in pygame.event.get():
         if event.type == pygame.QUIT or (keys[pygame.K_ESCAPE]):
             run = False
     
-    player.bounce2(player(10,20))
+    p1.bounce1()
+    p2.bounce2()
         
     screen.fill('black')
     
     for i in range(10):
         screen.blit(star.img,(random.randint(0,1250),random.randint(0,700)))
 
-    screen.blit(player.img,player.frect)
+    screen.blit(p1.img,p1.frect)
+    screen.blit(p2.img,p2.frect)
     pygame.display.update()
     
 
