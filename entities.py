c474db6a1d313806497c263a1ccb79c3033903a3
junaid-h0ssain <158271661+junaid-h0ssain@users.noreply.github.com>
2025-05-29 03:14:12 +0600

    made player bounce

--- entities.py
+++ entities.py
@@ -1,14 +1,45 @@
 import pygame
-import random
 
-pygame.init()
+screenHight = 720
+screenWidth = 1280
+screen = pygame.display.set_mode((screenWidth,screenHight))
+rb = 1
+
 class player:
-    img = pygame.image.load('images/player.png')#.convert_alpha()
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    
+    
+    img = pygame.image.load('images/player.png').convert_alpha()
+    frect = img.get_frect(center = (screenWidth/2,screenHight/2))
+    #frect = img.get_frect(center = ()))
     x_pos = 0
-    y_pos = 100
+    y_pos = 0    
+
+    def bounce1(self):
+        global rb
+        if player.frect.right <screenWidth and not rb:
+            player.frect.left+=1
+            # print(player.frect.right)
+            if player.frect.right==screenWidth:
+                rb = 1
+        if player.frect.left >0 and rb:
+            player.frect.right-=1
+            # print(player.frect.right)
+            if player.frect.left==0:
+                rb = 0
+    
+    def bounce2(self):
+        global rb
+        player.frect.x += rb * 1
+        if player.frect.right>screenWidth or player.frect.left<0:
+            rb*=-1
 
 class star:
-    img = pygame.image.load('images/star.png')#.convert_alpha()
+    img = pygame.image.load('images/star.png').convert_alpha()
     # for i in range(20):
     #     x_pos = random.randint(0,1250)
     #     y_pos = random.randint(0,700)
+
+# import main
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from entities import *
 
 pygame.init()
@@ -7,18 +8,24 @@ screenWidth = 1280
 screen = pygame.display.set_mode((screenWidth,screenHight))
 run = True
 
+
 while run:
-    # pygame.display.set_caption(f'{player.x_pos}, {player.y_pos}')
-    pygame.display.set_caption(str(pygame.display.get_current_refresh_rate()))
+    pygame.display.set_caption(f'{player.frect}')
+    # pygame.display.set_caption(str(pygame.display.get_current_refresh_rate()))
     keys = pygame.key.get_pressed()
     for event in pygame.event.get():
         if event.type == pygame.QUIT or (keys[pygame.K_ESCAPE]):
             run = False
     
+    player.bounce2(player(10,20))
+        
     screen.fill('black')
-    screen.blit(player.img,(player.x_pos,player.y_pos))
+    
     for i in range(10):
         screen.blit(star.img,(random.randint(0,1250),random.randint(0,700)))
+
+    screen.blit(player.img,player.frect)
     pygame.display.update()
+    
 
 pygame.quit()
