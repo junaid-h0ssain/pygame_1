ff45e369a9e693f856fd03f2dad707fcccd188f8
junaid-h0ssain <158271661+junaid-h0ssain@users.noreply.github.com>
2025-05-30 18:55:06 +0600

    added movement

--- entities.py
+++ entities.py
@@ -4,44 +4,54 @@ screenHight = 720
 screenWidth = 1280
 screen = pygame.display.set_mode((screenWidth,screenHight))
 
-
 class player:
     def __init__(self,x,y):
         self.x = x
         self.y = y
-        self.img = pygame.image.load('images/player.png').convert_alpha()
-        # frect = img.get_frect(center = (screenWidth/2,screenHight/2))
+        self.img = pygame.image.load('assets/images/player.png').convert_alpha()
         self.frect = self.img.get_frect(center = (x,y))
         self.rb = 1
-        self.speed = 300
-        # x_pos = 0
-        # y_pos = 0    
+        self.speed = 500
+        self.directionx = pygame.math.Vector2(1,0)
+        self.directiony = pygame.math.Vector2(0,1)   
 
     def bounce1(self):
-        
         if self.frect.right <screenWidth and not self.rb:
             self.frect.left+=1
-            # print(player.frect.right)
             if self.frect.right==screenWidth:
                 self.rb = 1
         if self.frect.left >0 and self.rb:
             self.frect.right-=1
-            # print(player.frect.right)
             if self.frect.left==0:
                 self.rb = 0
     
-    def bounce2(self):
-        
-        self.frect.x += self.rb * 1
+    def bounce2(self,dt):
+        self.frect.center += self.rb * self.speed*self.directionx*dt
+        self.frect.center += self.rb * self.speed*self.directionx*dt
         if self.frect.right>screenWidth or self.frect.left<0:
-            self.rb*=-1
+            #self.frect.right=screenWidth
+            # self.rb*=-1
+            self.directionx.x*=-1
+        if self.frect.bottom>screenHight or self.frect.top<0:
+            #self.frect.right=screenHight
+            # self.rb*=-1
+            self.directionx.y*=-1
+    
+    def movement(self,dt):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w] and self.frect.top>0:
+            self.frect.center-= self.speed*self.directiony*dt
+        if keys[pygame.K_s] and self.frect.bottom<screenHight:
+            self.frect.center+= self.speed*self.directiony*dt
+        if keys[pygame.K_d] and self.frect.right<screenWidth:
+            self.frect.center+= self.speed*self.directionx*dt
+        if keys[pygame.K_a] and self.frect.left>0:
+            self.frect.center-= self.speed*self.directionx*dt
 
 class star:
-    img = pygame.image.load('images/star.png').convert_alpha()
+    img = pygame.image.load('assets/images/star.png').convert_alpha()
 
 class laser:
     def __init__(self,x,y) -> None:
-        self.img = pygame.image.load('images/laser.png').convert_alpha()
+        self.img = pygame.image.load('assets/images/laser.png').convert_alpha()
         self.frect = self.img.get_frect(center = (x,y))
-
-# import main
--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from actions import shoot
 
 pygame.init()
 clock = pygame.time.Clock()
+fps = 60
 run = True
 p1 = player(100,100)
-# l1 = laser()
 
 sposx = []
 sposy = []
@@ -16,24 +16,16 @@ for i in range(20):
     sposy.append(random.randint(0,700))
 
 while run:
-    clock.tick(60)
+    dt = clock.tick(fps)/1000
+    #print(dt)
     pygame.display.set_caption(f'{pygame.time.Clock.get_fps(clock)}')
-    # pygame.display.set_caption(str(pygame.display.get_current_refresh_rate()))
     keys = pygame.key.get_pressed()
     for event in pygame.event.get():
         if event.type == pygame.QUIT or (keys[pygame.K_ESCAPE]):
             run = False
     
-    if keys[pygame.K_w] and p1.frect.top>0:
-        p1.frect.centery-= p1.speed*.1
-    if keys[pygame.K_s] and p1.frect.bottom<screenHight:
-        p1.frect.centery+= p1.speed*.1
-    if keys[pygame.K_d] and p1.frect.right<screenWidth:
-        p1.frect.centerx+= p1.speed*.1
-    if keys[pygame.K_a] and p1.frect.left>0:
-        p1.frect.centerx-= p1.speed*.1
-    
-    #p1.bounce2()
+    p1.movement(dt)
+    # p1.bounce2(dt)
         
     screen.fill('black')
     
